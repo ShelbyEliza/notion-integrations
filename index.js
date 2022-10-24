765a6074f9626e4011f3e6462e7810c029fbf6bf
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-10-24 13:31:50 -0700

    readme added, dotenv added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { Client } from "@notionhq/client";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
